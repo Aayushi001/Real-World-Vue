e2bcd94176d1a337285aa37e4f4231a55f054c50
Aayushi001 <Aayushi.riyasingh001@gmail.com>
2019-05-31 13:08:22 +0530

    installed vuex, added state and getters. Also saw hoe getters can be called inside a getter method

--- src/store.js
+++ src/store.js
@@ -4,7 +4,35 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: "abc123", name: "Aayushi Singh" },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community"
+    ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length;
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{user.name}}</h1>
+    <p>There are {{catLength}} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{cat}}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState(["user", "categories"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
